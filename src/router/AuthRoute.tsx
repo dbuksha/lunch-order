50f53e43f90f73b73b45eacfd9bccbaf7d6483bf
Daria Buksha <daria.buksha@distillery.com>
2021-04-13 12:05:06 +0300

    feat(app): preload data on auth

--- src/router/AuthRoute.tsx
+++ src/router/AuthRoute.tsx
@@ -1,13 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Redirect, Route, RouteProps } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { CircularProgress } from '@material-ui/core';
+
 import { RootState } from 'store/store';
+import { fetchDishes } from 'store/dishes';
+import { fetchLunches } from '../store/lunches';
 
 const AuthRoute: FC<RouteProps> = (props) => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(
     (state: RootState) => state.users.currentUser,
   );
+  const [isDataPreloaded, setIsDataPreloaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function preloadData() {
+      await dispatch(fetchDishes());
+      await dispatch(fetchLunches());
+      setIsDataPreloaded(true);
+    }
+
+    preloadData();
+  }, [dispatch]);
+
+  if (!isDataPreloaded) return <CircularProgress />;
 
   return (
     <Route
--- src/store/lunches/lunches-actions.ts
+++ src/store/lunches/lunches-actions.ts
@@ -5,7 +5,7 @@ import firebaseInstance, {
   getCollectionEntries,
 } from 'utils/firebase';
 import { Lunch } from 'entities/Lunch';
-import { Dish } from 'entities/Dish';
+import { DishesState } from '../dishes/dishes-reducer';
 
 enum ActionTypes {
   FETCH_LUNCHES = 'dishes/fetchLunches',
@@ -14,27 +14,21 @@ enum ActionTypes {
 const collectionRef = firebaseInstance.collection(Collections.Lunches);
 const dishesCollection = firebaseInstance.collection(Collections.Dishes);
 
-const fetchFilteredDishes = async (ids: string[]) => {
-  const data = await dishesCollection.where('id', 'in', ids).get();
-
-  return getCollectionEntries<Dish>(data);
-};
-
 export const fetchLunches = createAsyncThunk(
   ActionTypes.FETCH_LUNCHES,
-  async (dayNumber: number) => {
-    const data = await collectionRef.where('dayNumber', '==', dayNumber).get();
+  async (_, { getState }) => {
+    const {
+      dishes: { dishes: stateDishes },
+    } = getState() as { dishes: DishesState };
 
+    const data = await collectionRef.get();
     const dishSetCollection = getCollectionEntries<Lunch>(data);
 
-    const promises = dishSetCollection.map(async (lunch) => {
-      const dishesIds: string[] = lunch.dishes.map((dish: any) => dish.id);
-      let dishes = await fetchFilteredDishes(dishesIds);
-      dishes = dishes.map((d) => ({ ...d, selected: false }));
-
-      return { ...lunch, dishes };
+    return dishSetCollection.map((lunch) => {
+      return {
+        ...lunch,
+        dishes: lunch.dishes.map((d) => ({ ...stateDishes[d.id] })),
+      };
     });
-
-    return Promise.all(promises);
   },
 );
