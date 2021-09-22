fd1e9c2d6cd6cc61416da9f7f7932bf72a5c7796
dmshpakov <dmitry.shpakov@distillery.com>
2021-09-22 10:06:49 +0300

    checkbox and select were hid

--- src/pages/OrdersDelivery/OrdersDelivery.tsx
+++ src/pages/OrdersDelivery/OrdersDelivery.tsx
@@ -10,16 +10,12 @@ import {
   TableRow,
   createStyles,
   makeStyles,
-  Checkbox,
-  Select,
-  FormControlLabel,
 } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 
 import { getIsLoading } from 'store/app';
 import { fetchAllUsers, getAllUserSelector } from 'store/users';
 
-import DeleteAlert from 'components/AdminComponents/Alerts/DeleteAlert';
 import MainLayout from 'components/SiteLayout/MainLayout';
 import Ruble from 'components/Ruble';
 import DeliveryItem from './DeliveryItem';
@@ -72,28 +68,6 @@ const OrdersDelivery: FC = () => {
     );
   }
 
-  const toggleDialogHandler = (state: string) => () => {
-    if (deliveryCompleted) return;
-
-    // if (state === '') setPayer('default');
-
-    setDialogStatus(state);
-  };
-
-  const confirmDeliveryCompleted = () => {
-    setDeliveryCompleted(true);
-    toggleDialogHandler('')();
-  };
-
-  const handlePayerChange = (event: any) => {
-    setPayer(event.target.value as string);
-    setDialogStatus('payer');
-  };
-
-  const confirmSelectPayer = () => {
-    toggleDialogHandler('')();
-  };
-
   return (
     <MainLayout>
       <TableContainer component={Paper} className={classes.root}>
@@ -117,71 +91,16 @@ const OrdersDelivery: FC = () => {
               />
             ))}
             <TableRow>
-              <TableCell align="left">
+              <TableCell align="left" colSpan={3}>
                 <b className={classes.caption}>
                   Итого: {deliveryPrice}
                   <Ruble />
                 </b>
               </TableCell>
-              <TableCell align="right">
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={deliveryCompleted}
-                      onChange={toggleDialogHandler('delivery')}
-                      color="primary"
-                    />
-                  }
-                  label="Заказ сделан"
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">
-                <b>Получатель:</b>
-              </TableCell>
-              <TableCell align="right">
-                <Select
-                  native
-                  value={payer}
-                  onChange={handlePayerChange}
-                  variant="outlined"
-                  fullWidth
-                  inputProps={{
-                    name: 'user',
-                  }}
-                >
-                  <option value="default">Выберите пользователя</option>
-                  {users.length
-                    ? users.map((el: any) => (
-                        <option value={el.id}>{el.name}</option>
-                      ))
-                    : null}
-                </Select>
-              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
-      <DeleteAlert
-        status={dialogStatus !== ''}
-        title={`${
-          dialogStatus === 'delivery'
-            ? 'Вы уверены, что вы заказали еду по телефону?'
-            : 'Вы уверены, что хотите выбрать этого получателя?'
-        }`}
-        desc={`${
-          dialogStatus === 'delivery'
-            ? 'После того, как подтвердите действие, заказ еды на сегодняшний день будет недоступен'
-            : 'После того, как подтвердите действие изменить получателя будет невозможно'
-        }`}
-        closeAlert={toggleDialogHandler('')}
-        confirmEvent={
-          dialogStatus === 'delivery'
-            ? confirmDeliveryCompleted
-            : confirmSelectPayer
-        }
-      />
     </MainLayout>
   );
 };
